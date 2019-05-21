78704456c80a8fe7fc90e93cfe2ee2302fcbabe1
justine moreau <juhmoreau@gmail.com>
2019-05-21 17:35:45 +0200

    app working, missing css

---
+++ db/migrate/20190521102901_add_title_to_tasks.rb
@@ -0,0 +1,4 @@
+class AddTitleToTasks < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_101012) do
+ActiveRecord::Schema.define(version: 2019_05_21_103008) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
